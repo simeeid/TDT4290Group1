c1530823cd686e1bb8ff7a0580c9804531d2735a
javidr <javidr@ntnu.no>
2023-09-19 17:59:37 +0200

    Created Sensor configuration panel

---
+++ norbit-webapp/src/app/components/senssorConfiguration/SensorConfigurationPanel.tsx
@@ -0,0 +1,56 @@
+'use client'
+import React, { useState } from 'react';
+import style from './SensorConfigurationPanel.module.css';
+
+interface Props {
+  onConfigurationChange: (config: SensorConfig) => void;
+}
+
+export interface SensorConfig {
+  accelerometer: boolean;
+  temperature: boolean;
+  // Other sensors can be added here, e.g., temperature: boolean;
+}
+
+const SensorConfigurationPanel: React.FC<Props> = ({ onConfigurationChange }) => {
+  const [config, setConfig] = useState<SensorConfig>({ accelerometer: true, temperature: true });
+
+  const handleToggle = (sensor: keyof SensorConfig) => {
+    const newConfig = {
+      ...config,
+      [sensor]: !config[sensor],
+    };
+    setConfig(newConfig);
+    onConfigurationChange(newConfig);
+  };
+
+  return (
+    <div className= {style.labelBlock} >
+      <h3>Sensor Configuration</h3>
+      <div>
+        <label>
+          <input 
+            type="checkbox"
+            checked={config.accelerometer}
+            onChange={() => handleToggle('accelerometer')}
+          />
+          Show Accelerometer Chart
+        </label>
+
+        <label>
+        <input 
+          type="checkbox"
+          checked={config.temperature}
+          onChange={() => handleToggle('temperature')}
+        />
+        Show Temperature Chart
+      </label>
+
+        
+      </div>
+      {/* As more sensors are added, you can add more configuration options here */}
+    </div>
+  );
+};
+
+export default SensorConfigurationPanel;
