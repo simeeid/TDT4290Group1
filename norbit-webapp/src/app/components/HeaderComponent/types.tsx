beaeea5634ac8970020a7b9d5d538e5d134081c4
Olivia <johain@stud.ntnu.no>
2023-09-20 09:14:09 +0200

    Use semicolons in interfaces

--- norbit-webapp/src/app/components/HeaderComponent/types.tsx
+++ norbit-webapp/src/app/components/HeaderComponent/types.tsx
@@ -1,3 +1,3 @@
 export interface HeaderProps {
-  useSidebar: boolean
+  useSidebar: boolean;
 }
--- norbit-webapp/src/app/components/SidebarComponent/types.tsx
+++ norbit-webapp/src/app/components/SidebarComponent/types.tsx
@@ -1,6 +1,6 @@
 import {Device} from '../../DeviceManager';
 
 export interface SidebarProps {
-  devices?: Array<Device>,
-  connectDevice?: (newDevice: Device) => Promise<boolean>,
+  devices?: Array<Device>;
+  connectDevice?: (newDevice: Device) => Promise<boolean>;
 }
