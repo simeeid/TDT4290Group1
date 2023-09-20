10b77ec4ab79507884d3641403279507530c8e24
javidr <javidr@ntnu.no>
2023-09-20 09:13:10 +0200

    Corrected misspelling

--- norbit-webapp/src/app/components/dashboard/Dashboard.tsx
+++ norbit-webapp/src/app/components/dashboard/Dashboard.tsx
@@ -2,18 +2,18 @@
 import React, { useState } from 'react';
 import AccelerometerChart from '../accelerometer/AccelerometerChart';
 import SensorConfigurationPanel from '../sensorConfiguration/SensorConfigurationPanel';
-import dashboadStyles from './Dashboard.module.css';
+import dashboardStyles from './Dashboard.module.css';
 import { SensorConfig } from '../sensorConfiguration/types';
 const Dashboard: React.FC = () => {
   const [sensorConfig, setSensorConfig] = useState<SensorConfig>({ accelerometer: true, temperature: true });
 
   return (
-    <div className={dashboadStyles.dashboardContainer}>
-      <div className={dashboadStyles.chartsContainer}>
+    <div className={dashboardStyles.dashboardContainer}>
+      <div className={dashboardStyles.chartsContainer}>
         {sensorConfig.accelerometer && <AccelerometerChart />}
         {sensorConfig.temperature && <div>Tempreture</div>}
       </div>
-      <div className={dashboadStyles.configurationPanel}>
+      <div className={dashboardStyles.configurationPanel}>
         <SensorConfigurationPanel onConfigurationChange={setSensorConfig} />
       </div>
     </div>
