2cba12a58ef290a4c94c289d7ffeb28da7c48d5b
Olivia <johain@stud.ntnu.no>
2023-10-03 19:22:26 +0200

    Yeet more template storybook stuff, disable tailwind

--- norbit-webapp/src/app/page.tsx
+++ norbit-webapp/src/app/page.tsx
@@ -1,8 +1,9 @@
 import Dashboard from './components/dashboard/Dashboard'
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Welcome to our Home</h1>
+    <main className="flex flex-col items-center justify-between">
+      <h1 className="center-text">Welcome to our Home</h1>
       <>
         <Dashboard/>
       </>
