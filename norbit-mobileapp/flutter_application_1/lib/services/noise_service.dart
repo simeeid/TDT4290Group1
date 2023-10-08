fca118213f5e970800af1c6d75cb15c1141122e1
Julie Lundberg Suter <julielsu@stud.ntnu.no>
2023-10-08 13:00:43 +0200

    Real noise data implemented #58

--- norbit-mobileapp/flutter_application_1/lib/services/noise_service.dart
+++ norbit-mobileapp/flutter_application_1/lib/services/noise_service.dart
@@ -1,12 +1,43 @@
 import 'dart:async';
 import 'package:flutter_application_1/blocs/connectivity/noise_bloc.dart';
 import 'package:noise_meter/noise_meter.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class NoiseService {
-  NoiseService({required NoiseBloc noiseBloc}) {
-    noiseBloc.addNoise(7);
+  NoiseReading? latestReading;
+  StreamSubscription<NoiseReading>? noiseSubscription;
+  NoiseMeter? noiseMeter;
+  bool isRecording = false;
+  final NoiseBloc noiseBloc;
+
+  NoiseService({required this.noiseBloc});
+
+  void stop() {
+    noiseSubscription?.cancel();
+    isRecording = false;
   }
-  void TestService() {
-    print("service is connected");
+
+  void onData(NoiseReading noiseReading) {
+    latestReading = noiseReading;
+    print("THIIIIS ISSSS $noiseReading !!!!!!!!!!!!!!!!!!!!!");
+    noiseBloc.addNoise(noiseReading.meanDecibel.toDouble());
+  }
+
+  void onError(Object error) {
+    print(error);
+    stop();
+  }
+
+  Future<bool> checkPermission() async =>
+      await Permission.microphone.isGranted;
+
+  Future<void> requestPermission() async =>
+      await Permission.microphone.request();
+
+  Future<void> start() async {
+    noiseMeter ??= NoiseMeter();
+    if (!(await checkPermission())) await requestPermission();
+    noiseSubscription = noiseMeter?.noise.listen(onData, onError: onError);
+    isRecording = true;
   }
 }
--- norbit-mobileapp/flutter_application_1/lib/widgets/start_stop_button_widget.dart
+++ norbit-mobileapp/flutter_application_1/lib/widgets/start_stop_button_widget.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:provider/provider.dart';
 import '../blocs/connectivity/connectivity_bloc.dart';
 import '../blocs/connectivity/connectivity_state.dart';
+import 'package:flutter_application_1/services/noise_service.dart';
+import 'package:flutter_application_1/services/lux_service.dart';
+import 'package:flutter_application_1/services/accelerometer_service.dart';
 
 class StartStopButton extends StatelessWidget {
   final ConnectivityState state;
@@ -11,13 +15,17 @@ class StartStopButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final noiseService = Provider.of<NoiseService>(context, listen: false);
+    final luxService = Provider.of<LuxService>(context, listen: false);
+    final accelerometerService = Provider.of<AccelerometerService>(context, listen: false);
     return ElevatedButton(
       onPressed: state is Connected
           ? () async {
-              if (await Permission.microphone.request().isGranted) {
-                context.read<ConnectivityBloc>().add(StartStop());
-              }
-            }
+        if (await Permission.microphone.request().isGranted) {
+          context.read<ConnectivityBloc>().add(StartStop());
+        }
+        await noiseService.start();
+      }
           : null,
       style: ElevatedButton.styleFrom(
           primary: state is DataStarted ? Colors.red : Colors.green),
