ad002e2af4c5b1e1f07b76dd126196f388113585
javidr <javidr@ntnu.no>
2023-10-08 21:53:14 +0200

    Added Dashboard to home #57

--- norbit-webapp/pages/index.tsx
+++ norbit-webapp/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Dashboard from 'components/dashboard/Dashboard'
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <Dashboard/>
 
       </main>
 
