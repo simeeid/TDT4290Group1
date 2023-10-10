16261d1613910f7f60247eb895ae31ef013b456a
javidr <javidr@ntnu.no>
2023-10-10 23:34:44 +0200

    A small message subscription to cloud Relates #61

--- norbit-webapp/pages/index.tsx
+++ norbit-webapp/pages/index.tsx
@@ -1,8 +1,52 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Dashboard from 'components/dashboard/Dashboard'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Dashboard from 'components/dashboard/Dashboard';
+import { Amplify, PubSub } from 'aws-amplify';
+import { AWSIoTProvider } from '@aws-amplify/pubsub/lib/Providers';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [receivedMessage, setReceivedMessage] = useState<string>('');
+
+  const config = {
+    identityPoolId: process.env.NEXT_PUBLIC_IDENTITY_POOL_ID,
+    region: process.env.NEXT_PUBLIC_REGION,
+    userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID,
+    userPoolWebClientId: process.env.NEXT_PUBLIC_USER_POOL_WEB_CLIENT_ID,
+  };
+
+  Amplify.configure(config);
+  Amplify.addPluggable(
+    new AWSIoTProvider({
+      aws_pubsub_region: process.env.NEXT_PUBLIC_REGION,
+      aws_pubsub_endpoint: `wss://${process.env.NEXT_PUBLIC_MQTT_ID}/mqtt`,
+    })
+  );
+
+  useEffect(() => {
+    // Start subscription
+    const subscription = Amplify.PubSub.subscribe('real-time-weather').subscribe({
+      next: (data: any) => {
+        console.log('Message received', data.value.message);
+        setReceivedMessage(data.value.message); // Assuming the message is in data.value
+      },
+      error: (error: { message: string | string[] }) => {
+        console.error('Subscription error:', error);
+        if (error.message.includes('Connection failed')) {
+          console.error('Connection failed.');
+        }
+      },
+      close: () => console.log('Done'),
+    });
+
+    // Cleanup subscription on component unmount
+    return () => {
+      if (subscription) {
+        subscription.unsubscribe();
+      }
+    };
+  }, []); 
+  
   return (
     <div className={styles.container}>
       <Head>
@@ -12,13 +56,19 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+
+
+        {receivedMessage && (
+          <div>
+            <h2>Received Message:</h2>
+            <p>{receivedMessage}</p>
+          </div>
+        )}
         <Dashboard />
 
       </main>
 
-      <footer className={styles.footer}>
-
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
-  )
+  );
 }
