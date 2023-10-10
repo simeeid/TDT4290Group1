c913e3ada121f17650abf18649df8011c938ae94
Simen <simeeid@stud.ntnu.no>
2023-10-10 15:58:33 +0200

    Added new tests for accelerometer bloc
    Added tests for accelerometer bloc order
    
    Issue: #66

--- norbit-mobileapp/flutter_application_1/test/accelerometer_bloc_test.dart
+++ norbit-mobileapp/flutter_application_1/test/accelerometer_bloc_test.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_application_1/blocs/connectivity/accelerometer_bloc.dart';
 import 'package:sensors_plus/sensors_plus.dart';
+// ignore: depend_on_referenced_packages
+import 'package:async/async.dart';
 
 void main() {
   late AccelerometerBloc accelerometerBloc;
@@ -26,5 +28,16 @@ void main() {
     final event = AccelerometerEvent(1.0, 2.0, 3.0);
     accelerometerBloc.addAccelerometer(event);
     expect(accelerometerBloc.accelerometerStream, emits(event));
+  });
+
+  test('accelerometerStream emits multiple values in the correct order',
+      () async {
+    final queue = StreamQueue(accelerometerBloc.accelerometerStream);
+    final event1 = AccelerometerEvent(1.0, 2.0, 3.0);
+    final event2 = AccelerometerEvent(4.0, 5.0, 6.0);
+    accelerometerBloc.addAccelerometer(event1);
+    expect(await queue.next, event1);
+    accelerometerBloc.addAccelerometer(event2);
+    expect(await queue.next, event2);
   });
 }
