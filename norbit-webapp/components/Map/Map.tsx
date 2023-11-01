7529e341a44105a72ebeff0cee5a7088631b3810
javidr <javidr@ntnu.no>
2023-11-01 10:49:28 +0100

    Added pointer to Map Closses: Integrate location data in web #73

--- norbit-webapp/components/Map/Map.tsx
+++ norbit-webapp/components/Map/Map.tsx
@@ -14,11 +14,24 @@ if (typeof window !== 'undefined') {
 
 
 const LeafletMap: React.FC<{ amplifyInstance: TamplifyInstance | null }> = ({ amplifyInstance }) => {
+
   const mapRef = useRef(null);
   const mapInstance = useRef<L.Map | null>(null); 
   const [payload, setPayload] = useState<any>(null); 
   const [mapData, setMapData] = useState<MapData>({ latitude: 63, longitude: 10 });
   console.log(mapData);
+
+  if (L){
+    var greenIcon = L.icon({
+      iconUrl: 'pngegg.png',
+  
+      iconSize:     [20, 20], // size of the icon // size of the shadow
+      iconAnchor:   [20, 20], // point of the icon which will correspond to marker's location  // the same for the shadow
+      popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
+      });
+
+  }
+
 
   useEffect(() => {
     if (!payload) return;
@@ -34,7 +47,7 @@ const LeafletMap: React.FC<{ amplifyInstance: TamplifyInstance | null }> = ({ am
 
 
     // Add a marker
-    L.marker([mapData.latitude,mapData.longitude]).addTo(mapInstance.current);
+    L.marker([mapData.latitude,mapData.longitude], {icon: greenIcon}).addTo(mapInstance.current);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mapInstance.current);
 
