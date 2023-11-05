5e93a191f2063fcb20eb3ce430ff89a7f981169a
javidr <javidr@ntnu.no>
2023-11-05 17:38:29 +0100

    Added Signout button and username display Related: #82

--- norbit-webapp/components/HeaderComponent/HeaderComponent.tsx
+++ norbit-webapp/components/HeaderComponent/HeaderComponent.tsx
@@ -5,9 +5,28 @@ import React from 'react';
 import { SidebarComponent } from "../SidebarComponent/SidebarComponent";
 import { SidebarProps } from '../SidebarComponent/types'
 import { HeaderProps } from "./types"
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '@redux/store';
+import { userSignedOut } from '@redux/slices/amplifySlice';
+import { Auth } from 'aws-amplify';
+
 
 export const HeaderComponent: React.FC<HeaderProps & SidebarProps> = ({ useSidebar, ...props }) => {
   const mockAmplify = process.env["NEXT_PUBLIC_MOCK_AMPLIFY"] == "yes";
+
+  const isAuthenticated = useSelector((state: RootState) => state.amplify.isAuthenticated);
+  const user = useSelector((state: RootState) => state.amplify.userName);
+  const dispatch = useDispatch();
+  const handleSignOut = async () => {
+    try {
+      await Auth.signOut();
+      dispatch(userSignedOut());
+    } catch (error) {
+      console.error("Error signing out: ", error);
+    }
+  };
+  
+
 
   return (
     <header>
@@ -15,9 +34,20 @@ export const HeaderComponent: React.FC<HeaderProps & SidebarProps> = ({ useSideb
         {/* TODO: replace with image */}
         <p>Norbit</p>
       </div>
+      <div className="signout-button">
+        {isAuthenticated && <button onClick={handleSignOut}>Sign Out</button>}
+      </div>
+      <div className='user-and-menu'>
+        <div>
+          <p>
+            Hello {user}
+          </p>
+        </div>
       <div className="sidebar-button">
         { useSidebar ? <SidebarComponent {...props} amplifyInstance={mockAmplify ? null : Amplify} /> : ""}
       </div>
+      </div>
+
     </header>
   );
 }
